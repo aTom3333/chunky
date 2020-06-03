5100dc1cb826e5c0ce268577aec01045c02a69fd
Thomas Ferrand <a.tom.hic3@gmail.com>
2020-06-03 23:40:07 +0200

    Parse regio2d files (WIP)

--- chunky/src/java/se/llbit/chunky/world/cubicchunks/Region.java
+++ chunky/src/java/se/llbit/chunky/world/cubicchunks/Region.java
@@ -3,24 +3,87 @@ package se.llbit.chunky.world.cubicchunks;
 import se.llbit.chunky.world.Chunk;
 import se.llbit.chunky.world.ChunkDataSource;
 import se.llbit.chunky.world.ChunkPosition;
+import se.llbit.nbt.NamedTag;
+import se.llbit.nbt.Tag;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.InflaterInputStream;
 
 public class Region extends se.llbit.chunky.world.Region {
-  protected final String region2dFile;
-  protected final String region3dFile;
+  protected final String region2dFileName;
+  protected final String region3dFileName;
 
   public Region(ChunkPosition pos, World world) {
     super(pos, world);
-    region2dFile = String.format("");
-    region3dFile = String.format("");
+    region2dFileName = getRegion2dFile(pos);
+    region3dFileName = getRegion3dFile(pos);
   }
+
+  public static String getRegion2dFile(ChunkPosition pos) {
+    return String.format("%d.%d.2dr", pos.x, pos.z);
+  }
+
+  public static String getRegion3dFile(ChunkPosition pos) {
+    return String.format("%d.%d.%d.3dr", pos.x, pos.y, pos.z);
+  }
+
+  private World getWorld() {
+    return (World)world;
+  }
+
+  private static int SECTOR_SIZE = 512;
+  private static int HEADER_SIZE = 4096;
 
   @Override
   public synchronized void parse() {
-      throw new RuntimeException("Not implemented");
+    File region2dFile = new File(getWorld().getRegion2dDirectory(), region2dFileName);
+    File region3dFile = new File(getWorld().getRegion3dDirectory(), region3dFileName);
+    if(!region2dFile.isFile() || !region3dFile.isFile())
+      return;
+    long modtime = Math.max(region2dFile.lastModified(), region3dFile.lastModified());
+    if (regionFileTime == modtime) {
+      return;
+    }
+    regionFileTime = modtime;
+    try (RandomAccessFile file = new RandomAccessFile(region2dFile, "r")) {
+      long length = file.length();
+      if (length < HEADER_SIZE) {
+        System.err.println("Missing header in region file!");
+        return;
+      }
+      // TODO Do that elsewhere
+      int[] locations = new int[32*32];
+      for(int x = 0; x < 32; ++x) {
+        for(int z = 0; z < 32; ++z) {
+          locations[32*x+z] = file.readInt();
+        }
+      }
+      for(int x = 0; x < 32; ++x) {
+        for(int z = 0; z < 32; ++z) {
+          if(locations[x*32+z] == 0)
+            continue;
+          System.out.printf("Column at %d,%d\n", x, z);
+          long offset = (long)(locations[x*32+z] >> 8) * SECTOR_SIZE;
+          long size = (long)(locations[x*32+z] & 0xFF) * SECTOR_SIZE;
+          file.seek(offset);
+          int lengthByte = file.readInt();
+          byte[] buf = new byte[lengthByte];
+          file.read(buf);
+          InputStream in = new ByteArrayInputStream(buf);
+            in = new GZIPInputStream(in);
+          Tag parsedData = NamedTag.read(new DataInputStream(in));
+          System.out.println(parsedData.dumpTree());
+        }
+      }
+
+    } catch (IOException e) {
+      System.err.println("Failed to read region: " + e.getMessage());
+    }
+  }
+
+  protected void getColumnData(ChunkPosition pos) {
+
   }
 
   @Override
--- chunky/src/java/se/llbit/chunky/world/cubicchunks/World.java
+++ chunky/src/java/se/llbit/chunky/world/cubicchunks/World.java
@@ -9,18 +9,34 @@ import java.util.Set;
 import java.util.zip.ZipOutputStream;
 
 public class World extends se.llbit.chunky.world.World {
-    public World(String levelName, File worldDirectory, int dimension,
-                 Set<PlayerEntityData> playerEntities, boolean haveSpawnPos, long seed, long timestamp) {
-        super(levelName, worldDirectory, dimension, playerEntities, haveSpawnPos, seed, timestamp);
-    }
+  public World(String levelName, File worldDirectory, int dimension,
+               Set<PlayerEntityData> playerEntities, boolean haveSpawnPos, long seed, long timestamp) {
+    super(levelName, worldDirectory, dimension, playerEntities, haveSpawnPos, seed, timestamp);
+  }
 
-    @Override
-    protected void appendRegionToZip(ZipOutputStream zout, File regionDirectory, ChunkPosition regionPos, String regionZipFileName, Set<ChunkPosition> chunks) throws IOException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  protected void appendRegionToZip(ZipOutputStream zout, File regionDirectory, ChunkPosition regionPos, String regionZipFileName, Set<ChunkPosition> chunks) throws IOException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Region createRegion(ChunkPosition pos) {
-        return new Region(pos, this);
-    }
+  @Override
+  public Region createRegion(ChunkPosition pos) {
+    return new Region(pos, this);
+  }
+
+
+  public File getRegion2dDirectory() {
+    return new File(worldDirectory, "region2d");
+  }
+
+  public File getRegion3dDirectory() {
+    return new File(worldDirectory, "region3d");
+  }
+
+  @Override
+  public boolean regionExists(ChunkPosition pos) {
+    File regiond2dFile = new File(getRegion2dDirectory(), Region.getRegion2dFile(pos));
+    File regiond3dFile = new File(getRegion3dDirectory(), Region.getRegion3dFile(pos));
+    return regiond2dFile.exists() && regiond3dFile.exists();
+  }
 }
